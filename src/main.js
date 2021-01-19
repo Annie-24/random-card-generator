d73a2112f1df9a8fc0b047354e9e31e270155384
ana <anayin1@hotmail.com>
2021-01-19 18:11:30 -0400

    modificados archivos src/index.html, src/main.js, src/style.css, y demás archivos

--- src/main.js
+++ src/main.js
@@ -5,6 +5,46 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+// window.onload = () => {
+//   let randomSuit = generateRandomSuit();
+//   let randomNumber = generateRandomNumber();
+//   document.querySelector(".top-suit").innerHTML = randomSuit;
+//   document.querySelector(".bottom-suit").innerHTML = randomSuit;
+//   document.querySelector(".middle").innerHTML = randomNumber;
+// };
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumber];
+};
+let generateRandomSuit = () => {
+  let suit = ["♦", "♠", "♥", "♣"];
+
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
+};
+window.onload = function cardRandom() {
+  document.querySelector("body").style.background = "#OE7E12";
+  let card = document.querySelector(".card");
+  card.style.background = "white";
+  let top = document.querySelector(".top-suit");
+  top.style.justifyContent = "flex-start";
+  let middle = document.querySelector(".middle-suit");
+  middle.style.textAlign = "center";
+  middle.style.fontSize = "300px";
+  middle.style.paddingTop = "1cm";
+  let bottom = document.querySelector(".bottom-suit");
+  bottom.style.justifyContent = "flex-end";
+
+  middle.textContent = generateRandomNumber();
+  let suit = generateRandomSuit();
+  top.textContent = suit;
+  bottom.textContent = suit;
+
+  if (suit === "♥" || suit === "♦") {
+    card.style.color = "red";
+  } else {
+    card.style.color = "black";
+  }
+  document.getElementById("botton").addEventListener("click", cardRandom);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,9 @@ const WebpackErrorReporting = require('bc-webpack-error-reporting-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const port = 3000
-const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://')
-const publicUrl = `${port}-${host}`
-
+// const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://')
+const [schema, host] = ["http", "localhost"]; 
+const publicUrl = `${port}-{host}` 
 module.exports = {
   mode: 'development',
   entry: ['./src/main.js'],
